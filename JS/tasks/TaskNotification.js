dc335489e898d603bd74e6a8a6ef19a862cdd8a0
Yann Darwish <yann.darwish@gmail.com>
2022-08-04 11:17:39 +0200

    update notification background based on n of tasks

--- JS/tasks/TaskNotification.js
+++ JS/tasks/TaskNotification.js
@@ -6,6 +6,11 @@ class TaskNotification {
     render() {
         let myTasks = JSON.parse(localStorage.getItem('tasks'))
         let toDo = myTasks.filter(elt => elt.done === false)
+        if (toDo.length === 0) {
+            this.$wrapper.style.background = 'rgb(49, 168, 49)'
+        } else {
+            this.$wrapper.style.background = 'red'
+        }
         this.$wrapper.innerHTML = toDo.length
     }
 }
